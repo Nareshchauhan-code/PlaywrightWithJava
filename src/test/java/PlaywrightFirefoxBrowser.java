a1ad9688247c85487ea22177ec8c311f55e9da5c
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-05-01 18:57:38 +0530

    FireFox

---
+++ src/test/java/PlaywrightFirefoxBrowser.java
@@ -0,0 +1,28 @@
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.testng.annotations.Test;
+
+public class PlaywrightFirefoxBrowser {
+
+    @Test
+    void launchBrowser() {
+      BrowserType.LaunchOptions options = new BrowserType.LaunchOptions();
+        options.setChannel("firefox");
+        options.setHeadless(false);
+
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.firefox().launch(options);
+        Page page = browser.newPage();
+        page.navigate("https://google.com");
+        String title = page.title();
+        System.out.println(title);
+        String url = page.url();
+        System.out.println(url);
+        page.close();
+        playwright.close();
+
+    }
+
+}
